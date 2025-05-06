904480f971dd7c8dab43edde34ee96957b0c4f8c
OscarTinajero117 <tinajero117.oscar@gmail.com>
2025-05-06 16:02:58 -0600

    se agrega GenServer

--- lib/ladder/server.ex
+++ lib/ladder/server.ex
@@ -1,15 +1,27 @@
 defmodule Ladder.Server do
   use GenServer
 
-  alias Ladder.Board
+  alias Ladder.{Board, Words}
 
   # Client
-  # start_link
-  # make_move
+  def start_link({name, difficulty}) do
+    IO.puts("Starting for #{name}")
+    GenServer.start_link(__MODULE__, Words.initial_random_words(difficulty), name: name)
+  end
+
+  def turn(pid, word) do
+    GenServer.call(pid, {:turn, word})
+  end
 
   # Server
   def init(args) do
-    {:ok, Board.new(args)}
+    board = Board.new(args)
+
+    board
+    |> Board.show()
+    |> IO.puts()
+
+    {:ok, board}
   end
 
   # handle_call
