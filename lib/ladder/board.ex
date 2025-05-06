62b3ae3e79f6a37808529bc4f3431dbf298bb484
OscarTinajero117 <tinajero117.oscar@gmail.com>
2025-05-05 18:05:25 -0600

    se empieza a agregar la logica del juego

--- lib/ladder/board.ex
+++ lib/ladder/board.ex
@@ -1,16 +1,22 @@
 defmodule Ladder.Board do
+  defstruct [:answer, :moves]
   @words Ladder.Dictionary.words()
   @word_index Ladder.Dictionary.index_words(@words)
 
   def new({initial_word, answer}) do
-    # %__MODULE__{}
+    %__MODULE__{answer: answer, moves: [initial_word]}
   end
 
-  def guess(board, word) do
-    # ...word...
+  def turn(board, word) do
+    %{board | moves: [word | board.moves]}
   end
 
   def show(board) do
-    # "cats > mats ... ?"
+    moves =
+      board.moves
+      |> Enum.reverse()
+      |> Enum.join(" |> ")
+
+    moves <> "|> ... ?"
   end
 end
