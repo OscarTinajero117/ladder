7e48c1df9783746c34f4bba3a748c10f7b7d21fb
OscarTinajero117 <tinajero117.oscar@gmail.com>
2025-05-19 15:16:52 -0600

    implementación para funcionar con cli

--- lib/ladder.ex
+++ lib/ladder.ex
@@ -1,3 +1,22 @@
 defmodule Ladder do
-  #
+  alias Ladder.Server
+
+  @me __MODULE__
+
+  def play(difficulty \\ 2) do
+    Server.start_link({@me, difficulty})
+
+    next_turn(:ok)
+  end
+
+  def next_turn(:done), do: :complete
+
+  def next_turn(:ok) do
+    word =
+      IO.gets("Enter a word: ")
+      |> String.trim()
+
+    Server.turn(@me, word)
+    |> next_turn()
+  end
 end
